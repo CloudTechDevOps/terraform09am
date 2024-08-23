c8f6365587e1d1dbd56d5c08ec43c224b33287b4
veera <veerababu.narni232@gmail.com>
2024-08-23 10:19:43 +0530

    day-6-statefile updates

--- day-4/main.tf
+++ day-4/main.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "dev" {
-    bucket = "naresh-devops-awss"
+provider "aws" {
+  region = "us-east-1"
   
 }
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.dev.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+
+resource "aws_s3_bucket" "test" {
+  bucket = "awss3nareshdev"
+  
+  
 }
