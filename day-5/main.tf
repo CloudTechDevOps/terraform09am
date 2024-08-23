7bc0dd268d38e86ee807454e680804cc8709041b
veera <veerababu.narni232@gmail.com>
2024-08-23 09:37:05 +0530

    chnagesb made day-5

--- day-5/main.tf
+++ day-5/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test" {
     ami = "ami-0ae8f15ae66fe8cda"
     instance_type = "t2.micro"
     availability_zone = "us-east-1b"
-    key_name = "aws_key"
+    key_name = "awsaws"
     
   tags = {
     Name = "test"
