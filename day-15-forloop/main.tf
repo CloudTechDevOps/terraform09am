2db7cf0529611d0f8b1751113c960bb86573f21a
veera <veerababu.narni232@gmail.com>
2024-09-01 08:41:11 +0530

    updates terraform

---
+++ day-15-forloop/main.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "devops-project-veera" {
+  name        = "devops-project-veera"
+  description = "Allow TLS inbound traffic"
+
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000, 8082, 8081] : {
+      description      = "inbound rules"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-project-veera"
+  }
+}
